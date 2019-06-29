9ccbf53a0fcdf866adc9fa8d02cc8e62b1bf8afe
Yoshihiro Tanaka <contact@cordea.jp>
2019-06-29 17:13:04 +0900

    fix package

--- app/src/main/java/jp/cordea/coroutinechooser/example/MainActivity.kt
+++ app/src/main/java/jp/cordea/coroutinechooser/example/MainActivity.kt
@@ -1,7 +1,7 @@
-package jp.cordea.coroutinechooser
+package jp.cordea.coroutinechooser.example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
